dbfa119b89a63884cde157304235d6f99aba07a4
Miguel del Mazo <migueldelmazo@gmail.com>
2020-03-19 06:42:16 +0100

    Added more gifs

--- pages/video-cover/js/giphy.js
+++ pages/video-cover/js/giphy.js
@@ -6,7 +6,7 @@ const getUrl = () => {
   return (
     "//api.giphy.com/v1/gifs/search?" +
     "api_key=CNdqoaNxaFfWp6xIvfiSgWGz9n1z4a6T" +
-    "&q=ok" +
+    "&q=" + getQueryParam('giphy-search', 'ok') +
     "&limit=1" +
     "&offset=" +
     Math.floor(Math.random() * 100) +
@@ -26,6 +26,19 @@ const paintData = data => {
   imgEl.src = gifInfo.url;
   imgEl.width = Math.round(parseInt(gifInfo.width) * 1.5);
   imgEl.height = Math.round(parseInt(gifInfo.height) * 1.5);
+};
+
+// helpers: window location
+
+const getQueryParam = (key, defaultValue) => {
+  const params = window.location.search.substr(1).split('&');
+  for (let param of params) {
+    param = param.split('=');
+    if (param[0] === key) {
+      return param[1];
+    }
+  }
+  return defaultValue;
 };
 
 sendRequest();
